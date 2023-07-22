d0c2d9f494657ae4bbfed0e80a43cab9c58c9f83
luwesaram <luwesaram@gmail.com>
2023-07-22 17:12:11 +0800

    reorganized files, namespaces, and code to follow the mvvm pattern

--- userControls/loginHeaders.xaml.cs
+++ userControls/loginHeaders.xaml.cs
@@ -14,7 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace bookReviewConsoleApplication.userControls
+namespace bookReviewConsoleApplication.UserControls
 {
     /// <summary>
     /// Interaction logic for loginHeaders.xaml
