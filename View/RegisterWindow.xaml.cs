00ee4c64fb94be4919d0e864362e7b0fffe90453
luwesaram <luwesaram@gmail.com>
2023-07-23 14:29:26 +0800

    aligned login and register view to fit MVVM model

--- View/RegisterWindow.xaml.cs
+++ View/RegisterWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MySql.Data.MySqlClient;
+﻿using bookReviewConsoleApplication.ViewModel;
+using MySql.Data.MySqlClient;
 using MySqlX.XDevAPI.Common;
 using System;
 using System.Windows;
@@ -21,90 +22,7 @@ namespace bookReviewConsoleApplication
             string password = psBoxPass.Password;
             string confirm = psBoxConfirmPass.Password;
 
-            try {
-
-                if(!Conn.OpenConnection()) 
-                {
-                    MessageBox.Show("Unable to connect to the database.", "Error");
-                    return;
-                }
-
-                if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirm) || string.IsNullOrEmpty(email))
-                {
-                    MessageBox.Show("Please enter your username, email, and password!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-
-                else if (confirm != password)
-                {
-                    MessageBox.Show("Passwords do not match!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-
-                else if(UserExists(username, email))
-                {
-                    MessageBox.Show("User already Exists", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-                else
-                {
-                    MessageBox.Show("I am here");
-                    if(CreateUser(username, email, password))
-                    {
-                        // redirect to another window
-                    }
-                    else 
-                    {
-                        MessageBox.Show("Failed to create user");
-                    }
-
-                }
-            }
-            catch (MySqlException ex)
-            {
-                MessageBox.Show("Error: " + ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            finally
-            {
-                Conn.CloseConnection();
-            }
-        }
-
-        public bool UserExists(string username, string email)
-        {
-            bool exists = false;
-
-            try
-            {
-                string sql = "SELECT COUNT(*) FROM user WHERE username = @username OR email = @email";
-                MySqlCommand command = new MySqlCommand(sql, Conn.GetConnection());
-                command.Parameters.AddWithValue("@username", username);
-                command.Parameters.AddWithValue("@email", email);
-
-                int count = Convert.ToInt32(command.ExecuteScalar());
-                exists = count > 0;
-            }
-            catch (MySqlException error)
-            {
-                MessageBox.Show("Error: " + error.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            return exists;
-        }
-
-        public bool CreateUser(string username, string email, string password)
-        {
-            try
-            { 
-                string sql = $"INSERT INTO user (username, email, password) VALUES ('{username}', '{email}', '{password}')";
-                MySqlCommand command = new MySqlCommand(sql, Conn.GetConnection());
-
-                int rowsAffected = command.ExecuteNonQuery();
-                return rowsAffected > 0;
-            }
-            catch (MySqlException error)
-            {
-                MessageBox.Show("Error: " + error.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            return false;
+            RegisterViewModel registerModel = new RegisterViewModel(username, email, password, confirm, this);
         }
 
         private void lblExistUserDoubleClick(object sender, MouseButtonEventArgs e)
---
+++ ViewModel/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace bookReviewConsoleApplication.ViewModel
+{
+    public class RegisterViewModel
+    {
+
+        public RegisterViewModel(string username, string email, string password, string confirm, Window mainWindow)
+        {
+            RegisterMethod(username, email, password, confirm, mainWindow);
+        }
+
+        public void RegisterMethod(string username, string email, string password, string confirm, Window currentWindow)
+        {
+            UserManager registerUser = new UserManager(username,email,password,confirm,currentWindow);
+        }
+    }
+}
