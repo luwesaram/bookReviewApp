e4ad689ae3e68e9b356917b921cd479a595bd7c7
eosgnb <eosuganob@iskolarngbayan.pup.edu.ph>
2023-07-24 18:31:16 +0800

    Add logout function

--- View/UserPage.xaml.cs
+++ View/UserPage.xaml.cs
@@ -41,5 +41,19 @@ namespace bookReviewConsoleApplication
             }
 
         }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to logout?", "Logout Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                var currentWindow = Window.GetWindow(this);
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                currentWindow.Close();
+            }
+        }
     }
 }
