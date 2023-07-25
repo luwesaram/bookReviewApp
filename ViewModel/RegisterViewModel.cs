a17057e6bad200adce15af0355fa89748ed9810c
Emmanuel0725 <emmanuelfrancismacaya@gmail.com>
2023-07-25 18:27:18 +0800

    reformat login and register

--- ViewModel/RegisterViewModel.cs
+++ ViewModel/RegisterViewModel.cs
@@ -7,12 +7,7 @@ namespace bookReviewConsoleApplication.ViewModel
 
         public RegisterViewModel(string username, string email, string password, string confirm, Window mainWindow)
         {
-            RegisterMethod(username, email, password, confirm, mainWindow);
-        }
-
-        public void RegisterMethod(string username, string email, string password, string confirm, Window currentWindow)
-        {
-            UserManager registerUser = new UserManager(username, email, password, confirm, currentWindow);
+            UserManager registerUser = new(username, email, password, confirm, mainWindow);
         }
     }
 }
